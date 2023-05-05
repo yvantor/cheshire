be27e658380dff64facfe01ce9fb557fff027fe3
Cyril Koenig <cykoenig@iis.ee.ethz.ch>
2023-05-05 16:26:37 +0200

    fpga: Reworked fully FPGA flow, added support and CI for VCU128, added Vivado IP simulation flow
    fpga: Added ddr4 and vcu128 flow, added draft of Vivado IP simulation flow
    
    fpga: Added VIOs
    
    Connect VIO-generated reset signal to dram wrapper
    
    fpga: Support of zcu102
    
    fpga: zcu102.xdc constraint file added
    
    fpga: zcu102 changed phy and added firsts constraints
    
    fpga: Switching to clk_wiz and xilinx.mk
    
    fpga: Testing ddr4
    
    fpga: Start working on SPI driver
    Co-Authored-By: Yann Picod <ypicod@ethz.ch>
    
    fpga: SD card test
    
    fpga: Rolled-back SD
    
    fpga: Add vcu128 ci
    
    fpga: Debug new CI
    
    fpga: Adding artifacts management
    
    fpga: Added ID serializer in dram_wrapper and changed a few xdc constraints
    
    fpga: Correcting artifacts mngmt and applied new constraints to genesys2
    
    fpga: Corrected vivado sim script

--- sw/sw.mk
+++ sw/sw.mk
@@ -113,7 +113,7 @@ $(foreach link,$(patsubst $(CHS_SW_LD_DIR)/%.ld,%,$(wildcard $(CHS_SW_LD_DIR)/*.
 	$(CHS_SW_OBJCOPY) -O binary $< $@
 
 %.dtb: %.dts
-	$(CHS_SW_DTC) -I dts -O dtb -o $@ $<
+	$(CHS_SW_DTC) -I dts -O dtb -i $(CHS_SW_DIR)/boot -o $@ $<
 
 %.memh: %.elf
 	$(CHS_SW_OBJCOPY) -O verilog $< $@
@@ -137,7 +137,7 @@ $(foreach link,$(patsubst $(CHS_SW_LD_DIR)/%.ld,%,$(wildcard $(CHS_SW_LD_DIR)/*.
 CHS_CVA6_SDK_IMGS ?= $(addprefix $(CHS_SW_DIR)/deps/cva6-sdk/install64/,fw_payload.bin uImage)
 
 # Create full Linux disk image
-$(CHS_SW_DIR)/boot/linux.gpt.bin: $(CHS_SW_DIR)/boot/zsl.rom.bin $(CHS_SW_DIR)/boot/cheshire.dtb $(CHS_CVA6_SDK_IMGS)
+$(CHS_SW_DIR)/boot/linux-${BOARD}.gpt.bin: $(CHS_SW_DIR)/boot/zsl.rom.bin $(CHS_SW_DIR)/boot/cheshire_$(BOARD).dtb $(CHS_SW_DIR)/boot/install64/fw_payload.bin $(CHS_SW_DIR)/boot/install64/uImage
 	truncate -s $(CHS_SW_DISK_SIZE) $@
 	sgdisk --clear -g --set-alignment=1 \
 		--new=1:64:96 --typecode=1:$(CHS_SW_ZSL_TGUID) \
